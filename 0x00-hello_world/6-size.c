53baffdb600d79f64e8894aa7a7b87edd67559a8
Richard Muchira <rwachira.rw@gmail.com>
2023-03-16 06:05:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+printf("Size of a char: %lu\n byte(s)", sizeof(char);
+printf("Size of an int: %lu\n byte(s)", sizeof(int);
+printf("Size of a long int: %lu\n byte(s)", sizeof(long);
+printf("Size of a long long int: %lu\n byte(s)", sizeof(long long);
+printf("Size of a float: %lu\n byte(s)", sizeof(float);
+return (0);
+}
