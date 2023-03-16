ea2538eb1c6b63a2a695df0d927e922ba3edc976
Richard Muchira <rwachira.rw@gmail.com>
2023-03-16 06:35:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *
  * Return: Programming is like building a multilingual puzzle
  */
-int main()
+int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
