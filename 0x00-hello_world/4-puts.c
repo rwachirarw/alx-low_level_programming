6e827c6d51f87f941d45e8ed68b55c129bb1bbf4
Richard Muchira <rwachira.rw@gmail.com>
2023-03-16 05:30:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+char Mystr[] = "Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
