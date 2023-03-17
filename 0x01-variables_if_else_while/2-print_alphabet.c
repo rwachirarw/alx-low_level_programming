c487b4bbe1520fcfa66deb6bfb95b998c6c76cf1
Richard Muchira <rwachira.rw@gmail.com>
2023-03-17 06:44:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *** main - Entry point
+ ***
+ *** Return: Prints alphabets in lowercase
+ ***/
+int main(void)
+{
+for (int alph = 'a'; alph <= 'z'; alph++)
+	putchar(alph);
+	putchar('\n');
+}
