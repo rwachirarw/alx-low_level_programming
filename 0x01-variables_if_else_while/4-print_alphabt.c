4272128518e43599130268b1cbb03fed4deaee82
Richard Muchira <rwachira.rw@gmail.com>
2023-03-17 07:20:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *** main - Entry point
+ ***
+ *** Return: Prints alphabets in lowercase
+ ***/
+int main(void)
+{
+int alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+if (alph != 'e' && alph != 'q')
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
