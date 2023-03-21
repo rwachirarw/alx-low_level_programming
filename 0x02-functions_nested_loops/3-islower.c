1e49b15c54ec8f94e550eca31a6fc046ed9e83a9
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 12:15:47 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *** _islower - 1 if c is lowercase Returns 0 otherwise
+ *** Return 1 if c is lowercase Returns 0 otherwise
+ ***/
+
+int _islower(int c)
+{
+	if (c > 90)
+		return (1);
+		return (0);
+}
