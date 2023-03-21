2344388c3e6202f4a740c3e3dbbad3b7914f1111
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 09:38:05 +0000

    Amended 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
  *** main - Entry point
  *** print _putchar
  *** Return: 0
  ***/
+int _putchar(char);
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
 	return (0);
 }
