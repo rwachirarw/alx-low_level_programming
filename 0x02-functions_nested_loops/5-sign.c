897c1c1788c6fa03753ea3cb3d43403b65c6818e
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 14:20:50 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,23 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
-		_putchar(49);
 		_putchar(43);
-		_putchar('\n');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (0);
 		_putchar(48);
-		_putchar(48);
-		_putchar('\n');
+		return (1);
 	}
 	else
 	{
-		return (-1);
-		_putchar(49);
 		_putchar(45);
-		_putchar('\n');
+		return (1);
 	}
 }
