f6b79b6b10caca8768ba079a8790f6077709a5a9
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 15:43:33 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *** jack_bauer - computes the value of a interger
+ ***
+ *** Return: uhuh
+ **/
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+		putchar(i + 48);
+		for (i = 0; i < 4; i++)
+			_putchar(i + 48);
+			_putchar(':');
+			for (i = 0; i < 6; i++)
+				_putchar(i + 48);
+				for (i = 0; i < 10; i++)
+					_putchar(i + 48);
+return (0);
+}
