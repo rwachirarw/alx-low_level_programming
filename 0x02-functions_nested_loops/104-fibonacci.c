4f188bd73e135349a930ad96a48a0218ea5ad953
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 20:50:40 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,52 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - finds and prints the sum of the even=valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
-	unsigned long int num1, num2, next, sum;
+	int count;
+	unsigned long fb1 = 0, fb2 = 1, sum;
+	unsigned long fb1_hf1, fb1_hf2, fb2_hf1, fb2_hf2;
+	unsigned long hf1, hf2;
 
-	num1 = 1;
-	num2 = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; i++)
+	for (count = 0; count < 92; count++)
 	{
-		if (num1 < 4000000 && (num1 % 2) == 0)
-		{
-			sum = sum + num1;
-		}
-		next = num1 + num2;
-		num1 = num2;
-		num2 = next;
+		sum = fb1 + fb2;
+		printf("%lu, ", sum);
+
+		fb1 = fb2;
+		fb2 = sum;
 	}
 
-	printf("%lu\n", sum);
+	fb1_hf1 = fb1 / 10000000000;
+	fb2_hf1 = fb2 / 10000000000;
+	fb1_hf2 = fb1 % 10000000000;
+	fb2_hf2 = fb2 % 10000000000;
 
+	for (count = 93; count < 99; count++)
+	{
+		hf1 = fb1_hf1 + fb2_hf1;
+		hf2 = fb1_hf2 + fb2_hf2;
+		if (fb1_hf2 + fb2_hf2 > 9999999999)
+		{
+			hf1 += 1;
+			hf2 %= 10000000000;
+		}
+
+		printf("%lu%lu", hf1, hf2);
+		if (count != 98)
+			printf(", ");
+
+		fb1_hf1 = fb2_hf1;
+		fb1_hf2 = fb2_hf2;
+		fb2_hf1 = hf1;
+		fb2_hf2 = hf2;
+	}
+	printf("\n");
 	return (0);
 }
+
