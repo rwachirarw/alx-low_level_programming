d1e40d1fd201733d69f550c5715404dd0a73d946
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 14:54:17 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *** print_last_digit - computes the value of a interger
+ *** @n: bhabdh
+ *** Return: uhuh
+ **/
+int print_last_digit(int)
+{
+	int n;
+	int lastdig = n % 10;
+	int lastdigit;
+
+	_putchar(lastdig + '48');
+	lastdigit = _putchar(lastdig + '48');
+	return (lastdigit);
+}
