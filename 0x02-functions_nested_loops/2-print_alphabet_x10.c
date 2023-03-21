6864ebff6518a50dae275a619d95a559f556ab93
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 11:44:10 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *** print_alphabet - print alphabet
+ *** Return
+ ***/
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+	_putchar('\n');
+}
