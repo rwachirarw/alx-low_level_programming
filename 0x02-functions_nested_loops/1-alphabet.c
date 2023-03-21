4bb9196bc90a79c89a8e0d74f50d83428ba78fb9
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 10:24:05 +0000

    Removed the main function from 1-alphabet.c as it is meant to be a function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,13 @@
 #include "main.h"
 /**
  *** main - Entry point
- *** prints the alphabet, in lowercase, followed by a new line
+ *** A function prints the alphabet, in lowercase, followed by a new line
  *** Return: 0
  ***/
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
 void print_alphabet(void)
 {
 	char alph;
 	for(alph = 'a'; alph<='z'; alph++)
 		_putchar(alph);
+	_putchar('\n');
 }
