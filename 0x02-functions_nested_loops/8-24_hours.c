be5d17227598fb8c08b1f75d3b62712bb3b3e5fc
Richard Muchira <rwachira.rw@gmail.com>
2023-03-21 15:59:12 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,21 @@
  **/
 void jack_bauer(void)
 {
-	int i;
+	int i, j, k, l;
 
 	for (i = 0; i < 3; i++)
 	{
 		putchar(i + 48);
-		for (i = 0; i < 4; i++)
+		for (j = 0; j < 4; j++)
 		{
-			_putchar(i + 48);
+			_putchar(j + 48);
 			_putchar(':');
-			for (i = 0; i < 6; i++)
+			for (k = 0; k < 6; k++)
 			{
-				_putchar(i + 48);
-				for (i = 0; i < 10; i++)
+				_putchar(k + 48);
+				for (l = 0; l < 10; l++)
 				{
-					_putchar(i + 48);
+					_putchar(l + 48);
 					_putchar('\n');
 				}
 			}
