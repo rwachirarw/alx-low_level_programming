fc16bebdcc84885ed0da532de23598f2d9df9f95
Richard Muchira <rwachira.rw@gmail.com>
2023-03-23 06:39:14 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks for a digit (0 through 9)
+ * @a: interger to multiply
+ * @b: interger to multiply
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,7 @@
 #include <stdio.h>
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
