c95da8c10b47bc7f5a29269e14940ff0deb95fdc
Richard Muchira <rwachira.rw@gmail.com>
2023-03-23 07:00:42 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			putchar(i + 48);
+}
