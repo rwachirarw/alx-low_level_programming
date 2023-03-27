0cebc7996cedd1a6c49bb57097b158fb3fccb475
Richard Muchira <rwachira.rw@gmail.com>
2023-03-27 19:15:41 +0000

    Added the time.h prototype in the main.h file'

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,33 +1,36 @@
-#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define SUM 2772
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * main - it all starts here
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
-
-	sum = 0;
+	int sum = 0, num = 0, i = 0;
+	char str[100];
 
 	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	while (sum < SUM)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if (SUM - sum < 48)
+			sum -= str[--i];
+		else if (SUM -sum <= 126)
+			num = SUM - sum;
+		else
+			num = rand() % (126 - 48) + 48;
+		if (num)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			str[i++] = num;
+			sum += num;
 		}
+		num = 0;
 	}
-
+	str[i] = '\0';
+	printf("%s", str);
 	return (0);
 }
