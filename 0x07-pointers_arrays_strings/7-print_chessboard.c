9060e5102385318e8485ca5aa014f942a7b314dd
Richard Muchira <rwachira.rw@gmail.com>
2023-04-03 23:52:11 +0000

    101-crackme_password edited

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,12 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			if ((i + j) % 2 == 0)
-				_putchar((*(a + i))[j]);
-			else
-				_putchar(' ');
-		}
+			_putchar(a[i][j]);
 		_putchar('\n');
 	}
 }
