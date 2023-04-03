16543f50a5ec461d285944db8aefc88a8550f778
Richard Muchira <rwachira.rw@gmail.com>
2023-04-03 20:34:56 +0000

    Resolved a few logical errors

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,13 +10,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned in i, j;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j]
+			if (s[i] == accept[j])
 				break;
 		}
 		if (accept[j] == '\0')
