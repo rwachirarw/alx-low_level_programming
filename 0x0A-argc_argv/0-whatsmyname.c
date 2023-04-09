dfb27efdd58cee52f6aeb87a68dfa80f57d8342d
Richard Muchira <rwachira.rw@gmail.com>
2023-04-09 17:07:51 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+/**
+ *main-program start
+ *@argc: argument count
+ *@argv: argument array
+ *Return: interger
+ */
 
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
-}	
+}
