0ac6595914815d7a384d4f79c202353b043c3ace
Richard Muchira <rwachira.rw@gmail.com>
2023-04-09 21:46:43 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main-program start
+ *@argc: argument count
+ *@argv: argument array
+ *Return: interger
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0, c = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		argv[i];
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
