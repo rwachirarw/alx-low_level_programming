70a1dee26696c77302394bc059bb31345427ca96
Richard Muchira <rwachira.rw@gmail.com>
2023-04-09 22:31:29 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *main-program start
  *@argc: argument count
@@ -7,14 +6,11 @@
  *Return: interger
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, count = 0;
+	int i = 0;
 
 	for (i = 0; i < argc; i++)
-	{
-		count = *argv[i];
-	}
-	printf("%d\n", count);
+		printf("%s\n", argv[i]);
 	return (0);
 }
