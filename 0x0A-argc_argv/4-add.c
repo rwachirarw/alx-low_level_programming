4b64fee81a568468bc990606d482f041aa6fd8af
Richard Muchira <rwachira.rw@gmail.com>
2023-04-10 07:54:07 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main-program start
+ *@argc: argument count
+ *@argv: argument array
+ *Return: interger
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 0)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += strtol(argv[i], NULL, 10);
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
