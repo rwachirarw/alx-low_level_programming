99e14fa17b2aef3cd7fcc89eb7e6d894d660d209
Richard Muchira <rwachira.rw@gmail.com>
2023-04-13 18:34:55 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	new_string = malloc(sizeof(char) * (len1 + len2 + 1));
+	new_string = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (new_string == NULL)
 		return (NULL);
