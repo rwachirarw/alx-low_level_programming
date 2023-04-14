fff928ab635a1e3a08508ef80585be20117ee046
Richard Muchira <rwachira.rw@gmail.com>
2023-04-14 14:50:44 +0000

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,152 +1,46 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program start
- * @num1: first input
- * @num2: second input
+ * @argc: argument count
+ * @argv: argument vector
  * Return: multiplication of num1 and num2
  */
 
 int main(int argc, char **argv)
 {
-	char *num1;
-	char *num2;
-	char *result;
+	int num1, num2, i, mul;
 
 	if (argc != 3)
-		_print_error();
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	num1 = argv[1];
-	num2 = argv[2];
+	for (i = 0; argv[1][i]; i++)
+	{
+		if (argv[1][i] < '0' || argv[1][i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 
-	if (!_isdigit(num1) || !_isdigit(num2))
-		_print_error();
+	for (i = 0; argv[2][i]; i++)
+	{
+		if (argv[2][i] < '0' || argv[2][i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 
-	result = _multiply(num1, num2);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
-	if (result == NULL)
-		_print_error();
-
-	_print_result(result);
+	printf("%d\n", mul);
 
 	return (0);
-}
-
-/**
- *_multiply - multiplies two positive numbers
- *@num1: first number
- *@num2: second number
- *Return: Multiplication of num1 and num2
- */
-
-char *_multiply(char *num1, char *num2)
-{
-	int len1 = _strlen(num1);
-	int len2 = _strlen(num2);
-	int len_result = len1 + len2;
-	int i, j;
-	int carry, sum;
-	char *result;
-
-	result = malloc(sizeof(char) * (len_result + 1));
-	if (result == NULL)
-		return (NULL);
-
-	for (i = 0; i < len_result; i++)
-		result[i] = '0';
-	result[len_result] = '\0';
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			sum = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
-			carry = sum / 10;
-			result[i + j + 1] = (sum % 10) + '0';
-		}
-		result[i] += carry;
-	}
-
-	while (*result == '0' && *(result + 1) != '\0')
-		result++;
-
-	return (result);
-
-	free(result);
-}
-
-/**
- * _print_result - prints
- * @result: result to be printed
- * Return: void
- */
-
-void _print_result(char *result)
-{
-	_puts(result);
-	_putchar('\n');
-}
-
-/**
- * _print_error - prints error
- * Return: void
- */
-
-void _print_error(void)
-{
-	_puts("Error");
-	_putchar('\n');
-	exit(98);
-}
-
-/**
- * _puts - prints a string
- * @s: string to be printed
- * Return: void
- */
-
-void _puts(char *s)
-{
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-	}
-}
-
-/**
- *_isdigit - checks for a digit
- *@s: string to be checked
- *Return: 1
- */
-
-int _isdigit(char *s)
-{
-	while (*s != '\0')
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-
-	return (1);
-}
-/**
- * _strlen - checks for the length of a string
- * @s: string
- *Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
 }
