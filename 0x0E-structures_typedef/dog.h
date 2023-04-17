f478b3eefd418ae4f37e00767ed11eefd57b74b4
Richard Muchira <rwachira.rw@gmail.com>
2023-04-17 16:45:53 +0000

    Define struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+  *define a new type
+  */
+typedef struct dog dog_t;
 #endif
