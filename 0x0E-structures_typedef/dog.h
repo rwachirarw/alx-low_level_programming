86adfcc583f30972acd8e54a077fef9d392619bc
Richard Muchira <rwachira.rw@gmail.com>
2023-04-17 12:37:25 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog structure name
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: includes the dog details
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
