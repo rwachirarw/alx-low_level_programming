b3f2fd317e06f8d0dce7a823287f56373ad4c7d1
Richard Muchira <rwachira.rw@gmail.com>
2023-04-19 18:23:52 +0000

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,8 +18,6 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	/*if (!(s == "+" || s == "-" || s == "*" || s == "/" || s == "%"))
-		return (NULL);*/
 	i = 0;
 
 	while (ops[i].op != NULL)
@@ -29,5 +27,4 @@ int (*get_op_func(char *s))(int, int)
 		++i;
 	}
 	return (NULL);
-	
 }
