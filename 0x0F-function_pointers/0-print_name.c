9e4378bdd82d8ca0ddadcc5c182688fe72c9ef0c
Richard Muchira <rwachira.rw@gmail.com>
2023-04-19 08:54:35 +0000

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  * print_name - prints name
+  * @name: name
+  * @f: pointer to function
+  * Return: void
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
