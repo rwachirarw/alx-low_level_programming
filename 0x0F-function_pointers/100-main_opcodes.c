8d315fd5a55177fc424830ad9287992eca7657cf
Richard Muchira <rwachira.rw@gmail.com>
2023-04-19 18:40:16 +0000

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * print_opcodes - prints opcodes
-  * @addr: address
-  * @n: integer
-  * Return: void
-  */
-
-void print_opcodes(char *addr, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-		printf("%02hhx ", addr[i]);
-	printf("\n");
-}
-
-/**
-  * main - program start
-  * @argc: argument count
-  * @argv: argument vector
-  * Return: integer
-  */
+ * main - program start
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: integer
+ */
 
 int main(int argc, char **argv)
 {
@@ -30,17 +15,33 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
 	n = atoi(argv[1]);
 	if (n <= 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
 	print_opcodes((char *)main, n);
 
 	return (0);
+}
+
+/**
+ * print_opcodes - prints opcodes
+ * @addr: address
+ * @n: integer
+ * Return: void
+ */
+
+void print_opcodes(char *addr, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%02hhx ", addr[i]);
+	printf("\n");
 }
