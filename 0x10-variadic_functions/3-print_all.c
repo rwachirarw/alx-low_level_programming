b57efffd6400162542975e13b17daa1ababc4c51
Richard Muchira <rwachira.rw@gmail.com>
2023-04-20 18:26:31 +0000

    3-print_all.c fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-					printf("(nil)");
+					str = "(nil)";
 				printf("%s", str);
 				comma = true;
 				break;
