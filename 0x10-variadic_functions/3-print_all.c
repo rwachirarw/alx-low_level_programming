27f2f6a81bcb4491effafff8d3f50a6cb5d6f821
Richard Muchira <rwachira.rw@gmail.com>
2023-04-20 18:20:56 +0000

    3-print_all.c fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,10 +2,10 @@
 #include <stdarg.h>
 #include <stdbool.h>
 /**
-  * print_all - prints anything
-  * @format: format
-  * Return: void
-  */
+ * print_all - prints anything
+ * @format: format
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	int i = 0;
@@ -14,32 +14,32 @@ void print_all(const char * const format, ...)
 	bool comma;
 
 	va_start(args, format);
-	while (format[i])
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-			printf("%c", va_arg(args, int));
-			comma = true;
-			break;
+				printf("%c", va_arg(args, int));
+				comma = true;
+				break;
 			case 'i':
-			printf("%d", va_arg(args, int));
-			comma = true;
-			break;
+				printf("%d", va_arg(args, int));
+				comma = true;
+				break;
 			case 'f':
-			printf("%f", va_arg(args, double));
-			comma = true;
-			break;
+				printf("%f", va_arg(args, double));
+				comma = true;
+				break;
 			case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			printf("%s", str);
-			comma = true;
-			break;
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)");
+				printf("%s", str);
+				comma = true;
+				break;
 			default:
-			comma = false;
-			break;
+				comma = false;
+				break;
 		}
 
 		if (format[i + 1] != '\0' && comma == true)
