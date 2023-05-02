c68a595ade008eb12c61de21087d73e1abf245c5
Richard Muchira <rwachira.rw@gmail.com>
2023-05-02 22:10:03 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,62 +1,35 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-
-#define MAX_NODES 1000
+#include "lists.h"
 
 /**
- * struct node_info_s - Holds information about a node in a linked list.
- * @address: The address of the node in memory.
- * @value: The value stored in the node.
+ * print_listint_safe - Prints the elements of a listint_t linked list
+ * @head: A pointer to the head node
+ * Return: The number of nodes in the linked list.
  */
-typedef struct node_info_s
-{
-	const void *address;
-	int value;
-} node_info_t;
 
-/**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head of the list.
- *
- * Return: The number of nodes in the list.
- */
 size_t print_listint_safe(const listint_t *head)
 {
-	node_info_t node_info[MAX_NODES] = {0};
-	size_t count = 0;
+	const listint_t *visited[1024] = {NULL};
+	size_t count = 0, i;
 
-	while (head != NULL)
+	while (head)
 	{
-		const void *address = (const void *)head;
-		int value = head->n;
-		bool is_duplicate = false;
-
-		for (size_t i = 0; i < count; i++)
+		for (i = 0; i < count; i++)
 		{
-			if (node_info[i].address == address)
+			if (head == visited[i])
 			{
-				is_duplicate = true;
-				printf("-> [%p] %d\n", address, value);
-				break;
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (count);
 			}
 		}
 
-		if (!is_duplicate)
-		{
-			node_info[count].address = address;
-			node_info[count].value = value;
-			printf("[%p] %d\n", address, value);
-			count++;
-
-			if (count >= MAX_NODES)
-			{
-				fprintf(stderr, "Error: List is too long.\n");
-				exit(98);
-			}
-		}
-
+		visited[count++] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
+
+		if (count >= 1024)
+		{
+			exit(98);
+		}
 	}
 
 	return (count);
