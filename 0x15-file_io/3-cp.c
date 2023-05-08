2108230c59bbd6feac9b3ac013c0fa9506775dac
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 14:15:06 +0000

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * main - start of the program
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t n_read;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, buffer, n_read) != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		close(fd_to);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
