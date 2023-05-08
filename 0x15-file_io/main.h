62a5ca18d788c1ac0d3271f03eba29546c6f7225
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 20:58:54 +0000

    main.h betty fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -23,7 +23,6 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-	
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
