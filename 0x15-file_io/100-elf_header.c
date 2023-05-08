6cde47456ec015c48cc4396efc41808435114084
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 15:05:35 +0000

    100-elf_header.c fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,80 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <elf.h>
 
-/**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 98 on error
- */
-int main(int argc, char **argv)
-{
-	int fd;
-	ssize_t n;
-	Elf64_Ehdr ehdr;
+void print_error(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+	exit(98);
+}
 
-	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-		return (98);
+void print_elf_header(char* filename) {
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		print_error("Failed to open file");
 	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cannot read file %s\n", argv[1]);
-		return (98);
+
+	Elf64_Ehdr header;
+	if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+		print_error("Failed to read ELF header");
 	}
-	n = read(fd, &ehdr, sizeof(ehdr));
-	if (n != sizeof(ehdr))
-	{
-		dprintf(STDERR_FILENO, "Error: Not an ELF file - %s\n", argv[1]);
-		close(fd);
-		return (98);
+
+	// Check if the file is an ELF file
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
+		print_error("File is not an ELF file");
 	}
-	printf("Magic:   ");
-	for (n = 0; n < EI_NIDENT; n++)
-		printf("%02x ", ehdr.e_ident[n]);
+
+	// Print ELF header information
+	printf("Magic: ");
+	for (int i = 0; i < EI_NIDENT; i++) {
+		printf("%02x ", header.e_ident[i]);
+	}
 	printf("\n");
-	printf("Class:                             ");
-	printf("%s\n", ehdr.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:                              ");
-	printf("%s\n", ehdr.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
-	printf("Version:                           ");
-	printf("%d%s\n", ehdr.e_ident[EI_VERSION], ehdr.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	printf("OS/ABI:                            ");
-	switch (ehdr.e_ident[EI_OSABI])
-	{
+	printf("Class: %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("Version: %d\n", header.e_ident[EI_VERSION]);
+	printf("OS/ABI: ");
+	switch (header.e_ident[EI_OSABI]) {
 		case ELFOSABI_SYSV:
-			printf("UNIX - System V");
+			printf("UNIX - System V\n");
 			break;
 		case ELFOSABI_HPUX:
-			printf("UNIX - HP-UX");
+			printf("UNIX - HP-UX\n");
 			break;
 		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD");
+			printf("UNIX - NetBSD\n");
 			break;
 		case ELFOSABI_LINUX:
-			printf("UNIX - Linux");
+			printf("UNIX - Linux\n");
 			break;
 		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris");
+			printf("UNIX - Solaris\n");
 			break;
-		case ELFOSABI_IRIX:
-			printf("UNIX - IRIX");
+		case ELFOSABI_AIX:
+			printf("UNIX - AIX\n");
 			break;
 		case ELFOSABI_FREEBSD:
-			printf("UNIX - FreeBSD");
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_OPENBSD:
+			printf("UNIX - OpenBSD\n");
 			break;
 		default:
-			printf("<unknown: %x>", ehdr.e_ident[EI_OSABI]);
+			printf("<unknown>\n");
 			break;
 	}
-	printf("\n");
-	printf("ABI Version:                       ");
-	printf("%d\n", ehdr.e_ident[EI_ABIVERSION]);
+	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
+
+	printf("Type: ");
+	switch (header.e_type) {
+		case ET_NONE:
+			printf("NONE (Unknown type)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown>\n");
+			break;
+	}
+
+	printf("Entry point address: 0x%lx\n", header.e_entry);
+
 	close(fd);
-	return (0);
 }
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		print_error("Invalid arguments");
+	}
+
+	print_elf_header(argv[1]);
+
+	return 0;
+}
+
