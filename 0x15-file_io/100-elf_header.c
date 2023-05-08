3f60e220abaa8abe3c7c9fc19c1bb93ed761f244
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 14:58:35 +0000

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * print_elf_header - prints the information contained in an ELF header
+ * @ehdr: pointer to an ELF header
+ */
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+	ssize_t n;
+
+	printf("Magic:   ");
+	for (n = 0; n < EI_NIDENT; n++)
+		printf("%02x ", ehdr->e_ident[n]);
+	printf("\n");
+	printf("Class:                             ");
+	printf("%s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("Data:                              ");
+	printf("%s\n", ehdr->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
+	printf("Version:                           ");
+	printf("%d%s\n", ehdr->e_ident[EI_VERSION], ehdr->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+	printf("OS/ABI:                            ");
+	switch (ehdr->e_ident[EI_OSABI])
+	{
+		case ELFOSABI_SYSV:
+			printf("UNIX - System V");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD");
+			break;
+		default:
+			printf("<unknown: %x>", ehdr->e_ident[EI_OSABI]);
+			break;
+	}
+	printf("\n");
+	printf("ABI Version:                       ");
+	printf("%d\n", ehdr->e_ident[EI_ABIVERSION]);
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char **argv)
+{
+	int fd;
+	ssize_t n;
+	Elf64_Ehdr ehdr;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cannot read file %s\n", argv[1]);
+		return (98);
+	}
+	n = read(fd, &ehdr, sizeof(ehdr));
+	if (n != sizeof(ehdr))
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file - %s\n", argv[1]);
+		close(fd);
+		return (98);
+	}
+	print_elf_header(&ehdr);
+	close(fd);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <elf.h>
 
+	
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
@@ -15,5 +17,6 @@ int check_args(int argc);
 int open_files(char *file_from, char *file_to);
 int copy_file(int fd_from, int fd_to);
 void close_files(int fd_from, int fd_to);
+void print_elf_header(Elf64_Ehdr *ehdr);
 
 #endif
