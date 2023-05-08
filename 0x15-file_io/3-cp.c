8a2fedd892a6863b6dca122a232f363df10eaaec
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 18:29:44 +0000

    3-cp.c fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,39 @@
 #include "main.h"
 
 /**
- * main - start of the program
- * @argc: argument count
- * @argv: arguments
- * Return: 0 on success
+ * open_files - opens the source and destination files
+ * @argv: argument vector
+ * @fd_from: pointer to the source file descriptor
+ * @fd_to: pointer to the destination file descriptor
  */
-int main(int argc, char *argv[])
+void open_files(char **argv, int *fd_from, int *fd_to)
 {
-	int fd_from, fd_to;
-	ssize_t n_read;
-	char buffer[BUFFER_SIZE];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	*fd_from = open(argv[1], O_RDONLY);
+	if (*fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
-	if (fd_to == -1)
+	*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+	if (*fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(*fd_from);
 		exit(99);
 	}
+}
+
+/**
+ * copy_file - copies the contents of one file to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @argv: argument vector
+ */
+void copy_file(int fd_from, int fd_to, char **argv)
+{
+	ssize_t n_read;
+	char buffer[BUFFER_SIZE];
 
 	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -50,7 +53,15 @@ int main(int argc, char *argv[])
 		close(fd_to);
 		exit(98);
 	}
+}
 
+/**
+ * close_files - closes the source and destination files
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ */
+void close_files(int fd_from, int fd_to)
+{
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
@@ -63,6 +74,29 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	open_files(argv, &fd_from, &fd_to);
+
+	copy_file(fd_from, fd_to, argv);
+
+	close_files(fd_from, fd_to);
 
 	return (0);
 }
