2a3fa5bc2816f8b15a454ab93e3cc2b214418fd8
Richard Muchira <rwachira.rw@gmail.com>
2023-05-08 13:55:32 +0000

    1-create_file.c fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes_written;
-	size_t content_len = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		bytes_written = write(fd, text_content, content_len);
-		if (bytes_written == -1 && (size_t)bytes_written != content_len)
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written < 0)
 			return (-1);
 	}
 
