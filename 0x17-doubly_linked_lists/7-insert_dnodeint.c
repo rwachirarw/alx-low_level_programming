edf313d959475b5bcb6e92404266823a151401b6
Richard Muchira <rwachira.rw@gmail.com>
2023-06-15 18:59:35 +0000

    used temp != NULL instead of !temp)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
-	while (temp)
+	while (temp != NULL)
 	{
 		if (i == idx)
 		{
