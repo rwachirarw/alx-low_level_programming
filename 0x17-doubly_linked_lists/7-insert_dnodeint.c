9b400f0c45a9f2635fba070798e08a1a009d3464
Richard Muchira <rwachira.rw@gmail.com>
2023-06-15 19:08:17 +0000

    added the condition to check if the head is NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,31 +8,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h;
+	dlistint_t *temp = *h, *new_node;
 	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	while (temp != NULL)
 	{
 		if (i == idx)
 		{
-			if (!(*h))
-			{
+			if (*h == NULL)
 				*h = new_node;
-				return (new_node);
-			}
 			else if (temp == *h)
 			{
 				new_node->next = temp;
 				temp->prev = new_node;
 				*h = new_node;
-				return (new_node);
 			}
 			else
 			{
@@ -40,8 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node->next = temp;
 				temp->prev->next = new_node;
 				temp->prev = new_node;
-				return (new_node);
 			}
+			return (new_node);
 		}
 		temp = temp->next;
 		i++;
