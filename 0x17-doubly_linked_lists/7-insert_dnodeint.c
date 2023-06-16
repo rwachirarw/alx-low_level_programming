e9d5e6bc87b245ddc4b781a2e51fb82e2aca3cb3
Richard Muchira <rwachira.rw@gmail.com>
2023-06-16 01:59:46 +0000

    7-insert_dnodeint.c: Added the adding the node at the end of the list functionality

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,43 +8,41 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+
+	dlistint_t *new_node = NULL;
 	dlistint_t *temp = *h;
 	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-	while (temp != NULL)
+	if (idx == 0)
 	{
-		if (i == idx)
-		{
-			if (*h == NULL)
-			{
-				*h = new_node;
-				return (new_node);
-			}
-			else if (temp == *h)
-			{
-				new_node->next = temp;
-				temp->prev = new_node;
-				*h = new_node;
-				return (new_node);
-			}
-			else
-			{
-				new_node->prev = temp->prev;
-				new_node->next = temp;
-				temp->prev->next = new_node;
-				temp->prev = new_node;
-				return (new_node);
-			}
-		}
-		i++;
-		temp = temp->next;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	free(new_node);
-	return (NULL);
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
 }
