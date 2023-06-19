1eb2f539c1d7392b6c3ca48dd7a61d95a5974e67
Richard Muchira <rwachira.rw@gmail.com>
2023-06-19 07:15:08 +0000

    libdynamic.so: Created a dynamic library

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *** _isalpha - print alphabet
+ *** @c: bhabdh
+ *** Return: uhuh
+ **/
+
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
