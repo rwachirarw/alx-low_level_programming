bc2f0ab993d65719c1fbb18875f91042cdcad2f8
Richard Muchira <rwachira.rw@gmail.com>
2023-06-19 18:56:33 +0000

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/rwachirarw/alx-low_level_programming/blob/master/0x18-dynamic_libraries/crack.so
-export LD_PRELOAD=$PWD/crack.so
+wget -P ../ https://github.com/rwachirarw/alx-low_level_programming/blob/master/0x18-dynamic_libraries/crack.so
+LD_PRELOAD=../crack.so
