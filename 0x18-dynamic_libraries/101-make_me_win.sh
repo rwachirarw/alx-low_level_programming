5baff5feb9a00b3e4314e0f91c65e359e4d85c53
Richard Muchira <rwachira.rw@gmail.com>
2023-06-23 19:38:24 +0000

    101-make_me_win.sh: shared a github link to the crack.so file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P -o /tmp/crack.so https://github.com/
+wget -P -o /tmp/crack.so https://github.com/rwachirarw/alx-low_level_programming/raw/master/0x18-dynamic_libraries/crack.so
 export LD_PRELOAD=/tmp/crack.so
