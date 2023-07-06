7d5b6e53af08dd84bc4613dd569d0e19a3aee718
Richard Muchira <rwachira.rw@gmail.com>
2023-07-07 00:50:27 +0300

    changed the memory allocation to the size of a hash node when allocating or hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_node_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
