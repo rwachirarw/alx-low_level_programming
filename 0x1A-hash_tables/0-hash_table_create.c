c9db2796e923b0cf00403df0abbeafc75f3fed61
Richard Muchira <rwachira.rw@gmail.com>
2023-07-07 00:22:38 +0300

    Added condition to check for size < 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
 
+	if (size < 1)
+		return (NULL);	
+
 	ht = malloc(sizeof(hash_node_t));
 	if (ht == NULL)
 		return (NULL);
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *iter = NULL;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	iter = ht->array[index];
 	while (iter)
